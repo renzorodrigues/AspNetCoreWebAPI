938b2711a9b1db68db98de519027ea873cc826d2
Renzo Rodrigues <renzosantos@landix.com.br>
2019-09-13 09:36:28 -0300

    reiniciando projeot

--- webapi.Domain/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-namespace webapi.Domain.Entities
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- webapi.Domain/Services/UserService.cs
+++
@@ -1,7 +0,0 @@
-namespace webapi.Domain.Services
-{
-    public class UserService
-    {
-        
-    }
-}
